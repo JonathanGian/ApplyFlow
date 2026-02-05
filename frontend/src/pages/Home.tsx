7140a3ba11098c4c298482ff1766d6022e238285
Jonathan Gian <jongian90@gmail.com>
2026-02-05 11:06:58 +0200

    Add home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box>
+      <Typography variant="h4" align="center" mt={4}>
+        🎉 ApplyFlow is running!
+      </Typography>
+      </Box>
+  );
+}
