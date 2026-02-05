39f32a0c9f06e34489c5255bf0bdf06b97b2e6d7
Jonathan Gian <jongian90@gmail.com>
2026-02-05 10:45:47 +0200

    Create MUI theme and wrap App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './theme/theme.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </StrictMode>,
+  </ThemeProvider>,
 )
---
+++ frontend/src/theme/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme as muiCreateTheme } from '@mui/material/styles';
+
+export const theme = muiCreateTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+});
