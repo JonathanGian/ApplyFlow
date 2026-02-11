00d0d376c7c7a533666c47212f96730611181c63
Jonathan Gian <jongian90@gmail.com>
2026-02-11 14:02:06 +0200

    Update application.type.ts - Added DB Enum for stage column

--- ApplyFlow/types/applications/application.type.ts
+++ ApplyFlow/types/applications/application.type.ts
@@ -5,3 +5,5 @@ export type ApplicationRow = Database["public"]["Tables"]["applications"]["Row"]
 export type ApplicationInsert = Database["public"]["Tables"]["applications"]["Insert"];
 
 export type ApplicationUpdate = Database["public"]["Tables"]["applications"]["Update"];
+
+export type ApplicationStage = Database["public"]["Enums"]["application_stage"];
