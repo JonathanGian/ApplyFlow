94a47e3ce2a329534ff920750455de21955f7b4f
Jonathan Gian <jongian90@gmail.com>
2026-02-11 13:34:52 +0200

    Add application.type.ts - ApplicationRow ApplicationInsert ApplicationUpdate types added.

---
+++ ApplyFlow/app/types/applications/application.type.ts
@@ -0,0 +1,7 @@
+import { Database } from "../supabase/database.types";
+
+export type ApplicationRow = Database["public"]["Tables"]["applications"]["Row"];
+
+export type ApplicationInsert = Database["public"]["Tables"]["applications"]["Insert"];
+
+export type ApplicationUpdate = Database["public"]["Tables"]["applications"]["Update"];
